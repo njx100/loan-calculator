eada93fa9e3242c6474f22f689b0ccb1017c1430
njx100 <dnnguyen100@gmail.com>
2023-03-18 16:14:27 +0700

    completed project

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+const loanAmountEl = document.getElementById("loan-amount");
+const interestRateEl = document.getElementById("interest-rate");
+const monthsToPayEl = document.getElementById("months-to-pay");
+const monthlyPaymentEl = document.getElementById("monthly-payment");
 
+function updatePayment() {
+
+    const monthlyPayment = ((loanAmountEl.value) * (1+((interestRateEl.value) / 100)) / (monthsToPayEl.value)).toFixed(2);
+    monthlyPaymentEl.innerText = `$${monthlyPayment}`;
+};
+
+updatePayment();
+// loanAmountEl.addEventListener("change", updatePayment);
+// interestRateEl.addEventListener("change", updatePayment);
+// monthsToPayEl.addEventListener("change", updatePayment);
